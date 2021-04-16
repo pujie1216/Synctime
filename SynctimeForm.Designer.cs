51dca62003bdb526c9bed982f94c984dbed35763
pujie1216 <21360598+pujie1216@users.noreply.github.com>
2021-04-16 19:09:35 +0800

    First Code

---
+++ SynctimeForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Synctime
+{
+    partial class SynctimeForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.syncunitimebt = new System.Windows.Forms.Button();
+            this.localtimetlb = new System.Windows.Forms.Label();
+            this.localtimeflb = new System.Windows.Forms.Label();
+            this.unitimetlb = new System.Windows.Forms.Label();
+            this.unitimeflb = new System.Windows.Forms.Label();
+            this.localtimebgwk = new System.ComponentModel.BackgroundWorker();
+            this.unitimebgwk = new System.ComponentModel.BackgroundWorker();
+            this.jdtimetlb = new System.Windows.Forms.Label();
+            this.jdtimeflb = new System.Windows.Forms.Label();
+            this.syncjdtimebt = new System.Windows.Forms.Button();
+            this.jdtimebgwk = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // syncunitimebt
+            // 
+            this.syncunitimebt.Location = new System.Drawing.Point(12, 64);
+            this.syncunitimebt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.syncunitimebt.Name = "syncunitimebt";
+            this.syncunitimebt.Size = new System.Drawing.Size(112, 25);
+            this.syncunitimebt.TabIndex = 0;
+            this.syncunitimebt.Text = "点击同步联通时间";
+            this.syncunitimebt.UseVisualStyleBackColor = true;
+            this.syncunitimebt.Click += new System.EventHandler(this.Syncunitimebt_Click);
+            // 
+            // localtimetlb
+            // 
+            this.localtimetlb.AutoSize = true;
+            this.localtimetlb.Location = new System.Drawing.Point(12, 9);
+            this.localtimetlb.Name = "localtimetlb";
+            this.localtimetlb.Size = new System.Drawing.Size(59, 17);
+            this.localtimetlb.TabIndex = 1;
+            this.localtimetlb.Text = "本地时间:";
+            // 
+            // localtimeflb
+            // 
+            this.localtimeflb.AutoSize = true;
+            this.localtimeflb.Location = new System.Drawing.Point(77, 10);
+            this.localtimeflb.Name = "localtimeflb";
+            this.localtimeflb.Size = new System.Drawing.Size(0, 17);
+            this.localtimeflb.TabIndex = 2;
+            // 
+            // unitimetlb
+            // 
+            this.unitimetlb.AutoSize = true;
+            this.unitimetlb.Location = new System.Drawing.Point(12, 26);
+            this.unitimetlb.Name = "unitimetlb";
+            this.unitimetlb.Size = new System.Drawing.Size(59, 17);
+            this.unitimetlb.TabIndex = 3;
+            this.unitimetlb.Text = "联通时间:";
+            // 
+            // unitimeflb
+            // 
+            this.unitimeflb.AutoSize = true;
+            this.unitimeflb.Location = new System.Drawing.Point(77, 26);
+            this.unitimeflb.Name = "unitimeflb";
+            this.unitimeflb.Size = new System.Drawing.Size(0, 17);
+            this.unitimeflb.TabIndex = 4;
+            // 
+            // localtimebgwk
+            // 
+            this.localtimebgwk.WorkerSupportsCancellation = true;
+            this.localtimebgwk.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Localtimebgwk_DoWork);
+            // 
+            // unitimebgwk
+            // 
+            this.unitimebgwk.WorkerSupportsCancellation = true;
+            this.unitimebgwk.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Unitimebgwk_DoWork);
+            // 
+            // jdtimetlb
+            // 
+            this.jdtimetlb.AutoSize = true;
+            this.jdtimetlb.Location = new System.Drawing.Point(12, 43);
+            this.jdtimetlb.Name = "jdtimetlb";
+            this.jdtimetlb.Size = new System.Drawing.Size(59, 17);
+            this.jdtimetlb.TabIndex = 5;
+            this.jdtimetlb.Text = "京东时间:";
+            // 
+            // jdtimeflb
+            // 
+            this.jdtimeflb.AutoSize = true;
+            this.jdtimeflb.Location = new System.Drawing.Point(77, 43);
+            this.jdtimeflb.Name = "jdtimeflb";
+            this.jdtimeflb.Size = new System.Drawing.Size(0, 17);
+            this.jdtimeflb.TabIndex = 6;
+            // 
+            // syncjdtimebt
+            // 
+            this.syncjdtimebt.Location = new System.Drawing.Point(130, 64);
+            this.syncjdtimebt.Name = "syncjdtimebt";
+            this.syncjdtimebt.Size = new System.Drawing.Size(112, 25);
+            this.syncjdtimebt.TabIndex = 7;
+            this.syncjdtimebt.Text = "点击同步京东时间";
+            this.syncjdtimebt.UseVisualStyleBackColor = true;
+            this.syncjdtimebt.Click += new System.EventHandler(this.Syncjdtimebt_Click);
+            // 
+            // jdtimebgwk
+            // 
+            this.jdtimebgwk.WorkerSupportsCancellation = true;
+            this.jdtimebgwk.DoWork += new System.ComponentModel.DoWorkEventHandler(this.JDtimebgwk_DoWork);
+            // 
+            // SynctimeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 99);
+            this.Controls.Add(this.syncjdtimebt);
+            this.Controls.Add(this.jdtimeflb);
+            this.Controls.Add(this.jdtimetlb);
+            this.Controls.Add(this.unitimeflb);
+            this.Controls.Add(this.unitimetlb);
+            this.Controls.Add(this.localtimeflb);
+            this.Controls.Add(this.localtimetlb);
+            this.Controls.Add(this.syncunitimebt);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "SynctimeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "同步时间";
+            this.Load += new System.EventHandler(this.SynctimeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button syncunitimebt;
+        private System.Windows.Forms.Label localtimetlb;
+        private System.Windows.Forms.Label localtimeflb;
+        private System.Windows.Forms.Label unitimetlb;
+        private System.Windows.Forms.Label unitimeflb;
+        private System.ComponentModel.BackgroundWorker localtimebgwk;
+        private System.ComponentModel.BackgroundWorker unitimebgwk;
+        private System.Windows.Forms.Label jdtimetlb;
+        private System.Windows.Forms.Label jdtimeflb;
+        private System.Windows.Forms.Button syncjdtimebt;
+        private System.ComponentModel.BackgroundWorker jdtimebgwk;
+    }
+}
+
---
+++ SynctimeForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace Synctime
+{
+    public partial class SynctimeForm : Form
+    {
+        public SynctimeForm()
+        {
+            InitializeComponent();
+            Icon = System.Drawing.Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+        }
+
+        private void SynctimeForm_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            if (!localtimebgwk.IsBusy)
+            {
+                localtimebgwk.RunWorkerAsync();
+            }
+            if (!unitimebgwk.IsBusy)
+            {
+                unitimebgwk.RunWorkerAsync();
+            }
+            if (!jdtimebgwk.IsBusy)
+            {
+                jdtimebgwk.RunWorkerAsync();
+            }
+        }
+
+        private void Localtimebgwk_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                localtimeflb.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+
+        private static String Httpors()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            String httpors;
+            if (Convert.ToDouble(os.Version.ToString().Substring(0, 3)) < 6.1)
+            {
+                httpors = "http://";
+            }
+            else
+            {
+                httpors = "https://";
+            }
+            return httpors;
+        }
+
+        private static String Urlresp(String url)
+        {
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.Method = "GET";
+                httpWebRequest.UserAgent = "Mozilla/5.0 (Linux;Android 10;GM1910) AppleWebKit/537.36(KHTML, like Gecko) "
+                     + "Chrome / 83.0.4103.106 Mobile Safari/ 537.36";
+                httpWebRequest.Timeout = 1000;
+
+                String urlresp;
+                using (HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse)
+                {
+                    using (StreamReader StreamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                    {
+                        urlresp = StreamReader.ReadToEnd();
+                    }
+                }
+
+                return urlresp;
+            }
+            catch (Exception)
+            {
+                String urlresp = "error";
+                return urlresp;
+            }
+        }
+
+        private void Unitimebgwk_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            String httpors = Httpors();
+            String unifritimep = httpors + "m.client.10010.com/welfare-mall-front-activity/mobile/activity/getCurrentTimeMillis/v2";
+            while (true)
+            {
+                String unifritimepr = Urlresp(unifritimep);
+                if (unifritimepr != "error")
+                {
+                    JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                    Dictionary<String, Object> unifritimeprd = (Dictionary<String, Object>)javaScriptSerializer.DeserializeObject(unifritimepr);
+                    Object resdata = unifritimeprd["resdata"];
+                    Dictionary<String, Object> resdataD = (Dictionary<String, Object>)(resdata);
+                    Int64 currentTime = Int64.Parse(resdataD["currentTime"].ToString());
+                    unitimeflb.Text = startTime.AddMilliseconds(currentTime).ToString("yyyy-MM-dd HH:mm:ss.fff");
+                }
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+
+        private void JDtimebgwk_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            String httpors = Httpors();
+            String jdtimep = httpors + "api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5";
+            while (true)
+            {
+                String jdtimepr = Urlresp(jdtimep);
+                if (jdtimepr != "error")
+                {
+                    JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                    Dictionary<String, Object> jdtimeprd = (Dictionary<String, Object>)javaScriptSerializer.DeserializeObject(jdtimepr);
+                    Int64 serverTime = Int64.Parse(jdtimeprd["currentTime2"].ToString());
+                    jdtimeflb.Text = startTime.AddMilliseconds(serverTime).ToString("yyyy-MM-dd HH:mm:ss.fff");
+                }
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+
+        [DllImportAttribute("Kernel32.dll")]
+
+        public static extern void GetLocalTime(ref SystemTime systemTime);
+
+        [DllImportAttribute("Kernel32.dll")]
+
+        public static extern void SetLocalTime(ref SystemTime systemTime);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SystemTime
+        {
+            public UInt16 sysYear;
+            public UInt16 sysMonth;
+            public UInt16 sysDayOfWeek;
+            public UInt16 sysDay;
+            public UInt16 sysHour;
+            public UInt16 sysMinute;
+            public UInt16 sysSecond;
+            public UInt16 sysMilisecond;
+        }
+
+        private void SetSystime(String timef)
+        {
+            DateTime dateTime = Convert.ToDateTime(timef);
+            SystemTime systemTime = new SystemTime();
+            GetLocalTime(ref systemTime);
+            systemTime.sysYear = (UInt16)dateTime.Year;
+            systemTime.sysMonth = (UInt16)dateTime.Month;
+            systemTime.sysDay = (UInt16)dateTime.Day;
+            systemTime.sysHour = (UInt16)dateTime.Hour;
+            systemTime.sysMinute = (UInt16)dateTime.Minute;
+            systemTime.sysSecond = (UInt16)dateTime.Second;
+            systemTime.sysMilisecond = (UInt16)dateTime.Millisecond;
+            SetLocalTime(ref systemTime);
+        }
+
+        private void Syncunitimebt_Click(object sender, EventArgs e)
+        {
+            String unitimef = unitimeflb.Text;
+            SetSystime(unitimef);
+            MessageBox.Show("本地时间已同步联通时间", "提示");
+        }
+
+        private void Syncjdtimebt_Click(object sender, EventArgs e)
+        {
+            String jdtimef = jdtimeflb.Text;
+            SetSystime(jdtimef);
+            MessageBox.Show("本地时间已同步京东时间", "提示");
+        }
+    }
+}
